fa9af172452f6e2d5d7c571edaff5e3b6ca4785b
madhu <madhu@madhu>
2025-02-01 20:13:46 +0530

    first code

---
+++ javatns/src/com/tnsif/packages/Demo.java
@@ -0,0 +1,13 @@
+package com.tnsif.packages;
+
+public class Demo {
+	int a=9;
+
+	public static void main(String[] args) {
+		Demo d=new Demo();
+		System.out.println(d.a);
+	
+
+	}
+
+}
---
+++ javatns/src/com/tnsif/packages/Example.java
@@ -0,0 +1,12 @@
+package com.tnsif.packages;
+
+public class Example {
+
+	public static void main(String[] args) {
+		Demo d=new Demo();
+		System.out.println(d.a);
+		// TODO Auto-generated method stub
+
+	}
+
+}
